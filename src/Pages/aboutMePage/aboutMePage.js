a9e9d0ba53b1364def5d63783915cf6889e94805
Aayush <aayushtapadia28@gmail.com>
2024-10-30 19:16:29 +0530

    second commit

--- src/Pages/aboutMePage/aboutMePage.js
+++ src/Pages/aboutMePage/aboutMePage.js
@@ -3,6 +3,7 @@ import AboutMe from "../../components/aboutMe/aboutMe"
 const AboutMePage=({route,navigation})=>{
     const { formData } = route.params;
     console.log('form data in about me data',formData)
+    console.log('form data in about me datas i sjdjo',formData)
 return (
     <>
     <AboutMe aboutMe={formData} navigation={navigation}/>
